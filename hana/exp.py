00f497b634d80716ddb3971bc8fab5e05b23df9d
Marcel Jankrift <marcel@jankrift.de>
2019-11-22 19:50:00 +0100

    drop and read statistics

--- hana/exp.py
+++ hana/exp.py
@@ -1,4 +1,5 @@
 import pyhdb
+import json
 
 
 class DatabaseConnector:
@@ -12,9 +13,31 @@ class DatabaseConnector:
 
         self.cursor = self.connection.cursor()
 
+    def drop_all_statistics(self):
+        print('drop all statistics')
+        self.run('drop statistics on indexselection_tpch___1.lineitem')
+        self.run('drop statistics on tpchsf1_test_import.lineitem')
+
+    def run(self, statement):
+        self.cursor.execute(statement)
+        return self.cursor.fetchone()
+
+    def get_statistics(self):
+        try:
+            result = self.run('select count(*) as c, data_statistics_type, data_source_schema_name, data_source_object_name from M_DATA_STATISTICS group by data_statistics_type, data_source_schema_name, data_source_object_name')
+            print(result)
+        except:
+            print('no statistics found')
+
 
 def main():
-    db_conn = DatabaseConnector()
+    with open('connection.json', 'r') as file:
+        connection_data = json.load(file)
+    db_conn = DatabaseConnector(*connection_data)
+    db_conn.get_statistics()
+    db_conn.drop_all_statistics()
+    db_conn.get_statistics()
+
 
 if __name__ == '__main__':
     main()
