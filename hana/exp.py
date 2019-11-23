fa181ae7f184ddc10699931b038c0541b7314e94
Marcel Jankrift <marcel@jankrift.de>
2019-11-23 12:54:40 +0100

    export and import statistics

--- hana/exp.py
+++ hana/exp.py
@@ -20,20 +20,35 @@ class DatabaseConnector:
 
     def run(self, statement):
         self.cursor.execute(statement)
-        return self.cursor.fetchone()
+        return self.cursor.fetchall()
 
     def get_statistics(self):
+        print('show statistics')
         try:
             result = self.run('select count(*) as c, data_statistics_type, data_source_schema_name, data_source_object_name from M_DATA_STATISTICS group by data_statistics_type, data_source_schema_name, data_source_object_name')
             print(result)
-        except:
+        except Exception:
             print('no statistics found')
+
+    def create_statistics_data_schema(self):
+        print('create statistics')
+        self.run('create statistics on indexselection_tpch___1.lineitem type histogram REFRESH TYPE manual enable on')
+        print('create statistics done')
+
+    def export_statistics(self):
+        print('export')
+        self.run("export indexselection_tpch___1.lineitem as csv into '/usr/sap/JA1/HDB00/work/newexport' with replace all statistics only")
+        print('import')
+        self.run("import indexselection_tpch___1.lineitem as csv from '/usr/sap/JA1/HDB00/work/newexport' with rename schema indexselection_tpch___1 to tpchsf1_test_import replace statistics only")
 
 
 def main():
     with open('connection.json', 'r') as file:
         connection_data = json.load(file)
     db_conn = DatabaseConnector(*connection_data)
+    db_conn.get_statistics()
+    db_conn.create_statistics_data_schema()
+    db_conn.export_statistics()
     db_conn.get_statistics()
     db_conn.drop_all_statistics()
     db_conn.get_statistics()
