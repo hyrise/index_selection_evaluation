aaf5342017d5659c90e9dd85eb2cdd16fb80eda6
Marcel Jankrift <marcel@jankrift.de>
2020-01-10 17:04:50 +0100

    refactor dexter output parsing

--- selection/algorithms/dexter_algorithm.py
+++ selection/algorithms/dexter_algorithm.py
@@ -1,11 +1,10 @@
 from ..selection_algorithm import SelectionAlgorithm
+from ..index import Index
 import subprocess
 import logging
 import os
 
 
-# TODO add parameter for single_column / multi_column / winning
-# and adjust dexter algorithm
 DEFAULT_PARAMETERS = {'min_saving_percentage': 50}
 
 
@@ -18,8 +17,7 @@ class DexterAlgorithm(SelectionAlgorithm):
         min_percentage = self.parameters['min_saving_percentage']
         database_name = self.database_connector.db_name
 
-        potential_indexes = self.potential_indexes(workload)
-        indexes_found = []
+        index_columns = []
 
         for query in workload.queries:
             command = (f'dexter {database_name}'
@@ -40,13 +38,16 @@ class DexterAlgorithm(SelectionAlgorithm):
 
             if 'public.' in output_string:
                 index = output_string.split('public.')[1].split(' (')
-                table = index[0]
-                column = index[1].split(')')[0]
-                indexes_found.append(f'{table};{column}')
-            # TODO support multicolumn indices
-        indexes = []
-        for index in potential_indexes:
-            column = index.columns[0]
-            if f'{column.table.name};{column.name}' in indexes_found:
-                indexes.append(index)
-        return indexes
+                table_name = index[0]
+                column_names = index[1].split(')')[0].split(', ')
+                columns = []
+                for column_name in column_names:
+                    column_object = next((c for c in query.columns
+                                          if c.name == column_name
+                                          and c.table.name == table_name),
+                                         None)
+                    columns.append(column_object)
+                # Check if the same index columns already in list
+                if columns not in index_columns:
+                    index_columns.append(columns)
+        return [Index(c) for c in index_columns]
