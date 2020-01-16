87c85dc7916add182552ee234f463520edf72e5c
Marcel Jankrift <marcel@jankrift.de>
2020-01-16 18:49:08 +0100

    remove comments

--- selection/algorithms/microsoft/candidate_index_selection.py
+++ selection/algorithms/microsoft/candidate_index_selection.py
@@ -13,7 +13,6 @@ class CandidateIndexSelection():
         candidates = []
         for query in workload.queries:
             logging.debug('Find candidates for query\t{}...'.format(query))
-            # indexable_columns = self._get_indexable_columns(query)
             # Create a workload consisting of one query
             query_workload = Workload([query], workload.database_name)
             indexes = self._filter_potential_indexes(query, potential_indexes)
@@ -21,7 +20,6 @@ class CandidateIndexSelection():
                                                    query_workload,
                                                    self.cost_evaluation,
                                                    self.microsoft_algorithm,
-                                                   # TODO set True again
                                                    candidate_selection=True)
             candidates.extend(enumeration.enumerate())
         # Remove duplicates from candidates list
@@ -29,10 +27,6 @@ class CandidateIndexSelection():
         logging.info('Number of candidates: {}'.format(len(candidates)))
         logging.info('Candidates: {}'.format(candidates))
         return candidates
-
-    def _get_indexable_columns(self, query):
-        # TODO: only relevant columns... (maybe multiple options)
-        return query.indexable_columns()
 
     def _filter_potential_indexes(self, query, potential_indexes):
         indexes = []
