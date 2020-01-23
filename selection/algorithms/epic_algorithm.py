7113e896f04c05bc6b562dc866a14d7a37523ceb
Marcel Jankrift <marcel@jankrift.de>
2020-01-23 13:26:23 +0100

    cleanup epic

--- selection/algorithms/epic_algorithm.py
+++ selection/algorithms/epic_algorithm.py
@@ -4,7 +4,7 @@ import logging
 
 
 DEFAULT_PARAMETERS = {'cost_estimation': 'whatif', 'budget': 10,
-                      'number_steps': 20, 'pruning': True}
+                      'pruning': True}
 
 
 class EPICAlgorithm(SelectionAlgorithm):
@@ -16,39 +16,24 @@ class EPICAlgorithm(SelectionAlgorithm):
 
     def _calculate_best_indexes(self, workload):
         logging.info('Calculating best indexes EPIC')
+        single_attribute_index_candidates = self.potential_indexes(workload)
 
+        # Current index combination
         index_combination = []
-        initial_cost = self._retrieve_cost(workload, index_combination)
-        # # ?
-        # initial_reconfig_cost = self._retrieve_reconfig_cost(indexes)
-        best = [[], 0, initial_cost]
+        index_combination_size = 0
+        # Best index combination during evaluation step
+        best = {'combination': [],
+                'cost': self._retrieve_cost(workload, index_combination),
+                'benefit_to_size_ratio': 0}
 
-        singleattr_index_candidates = self.potential_indexes(workload)
-        step = 0
-        combi_size = None
-
-        # while step < self.parameters['number_steps']:
         # Budget is used here. or breaking when no cost improvement
         while True:
-            print(f'step: {step}')
-            print(index_combination)
-            # import pdb;pdb.set_trace()
-
-            # make following optional
-            # attributes = [item for sublist in index_combination
-            #               for item in sublist.columns]
-            # singleattr_index_candidates = [x for x
-            #                                in singleattr_index_candidates
-            #                                if x.columns[0] not in attributes]
-            # --------
-            print('combi size', combi_size)
-
-            for candidate in singleattr_index_candidates:
-                if combi_size:
-                    est_size = candidate.estimated_size
-                    if combi_size + est_size > self.budget:
-                        # print('continue')
-                        continue
+            for candidate in single_attribute_index_candidates:
+                # Candidate not used anymore if too large for budget
+                if (candidate.estimated_size and index_combination_size
+                        + candidate.estimated_size > self.budget):
+                    single_attribute_index_candidates.remove(candidate)
+                    continue
 
                 if candidate not in index_combination:
                     self._evaluate_combination(index_combination + [candidate],
@@ -66,33 +51,32 @@ class EPICAlgorithm(SelectionAlgorithm):
                         new_combination[i] = new_index
                         self._evaluate_combination(new_combination, best,
                                                    workload)
-            if best[1] == 0:
+            if best['benefit_to_size_ratio'] == 0:
                 break
-            index_combination = best[0]
-            # Reset ratio for new step
-            best[1] = 0
-            step += 1
-            combi_size = sum(x.estimated_size for x in index_combination)
+            index_combination = best['combination']
+            best['benefit_to_size_ratio'] = 0
+            index_combination_size = sum(x.estimated_size
+                                         for x in index_combination)
         return index_combination
 
     def _evaluate_combination(self, index_combination, best, workload):
         cost = self._retrieve_cost(workload, index_combination)
-        benefit = best[2] - cost
+        benefit = best['cost'] - cost
         size = sum(x.estimated_size for x in index_combination)
         ratio = benefit / size
 
-        if ratio > best[1] and size <= self.budget:
-            logging.debug(f'new best cost: {cost}\t{size}')
-            best[0] = index_combination
-            best[1] = ratio
-            best[2] = cost
+        if ratio > best['benefit_to_size_ratio'] and size <= self.budget:
+            logging.debug(f'new best cost and size: {cost}\t{size}')
+            best['combination'] = index_combination
+            best['cost'] = cost
+            best['benefit_to_size_ratio'] = ratio
 
     def _retrieve_cost(self, workload, indexes):
         cost = self.cost_evaluation.calculate_cost(workload, indexes,
                                                    store_size=True)
         return cost
 
+    # The cost to get to the new index combination, i.e. cost of
+    # adding new indexes and dropping existing unneeded indexes
     def _retrieve_reconfig_cost(self, new, existing_indexes=[]):
-        # TODO how?
-        # size * ?
         return 0
