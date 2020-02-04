741c4994487326a6b36f5f46971abcce167ea327
Jan Kossmann <jan.kossmann@hpi.de>
2020-02-04 18:34:41 +0100

    Add test_cost_evaluation

---
+++ tests/test_cost_evaluation.py
@@ -0,0 +1,158 @@
+from selection.cost_evaluation import CostEvaluation
+from selection.index import Index
+from selection.workload import Column, Query, Table, Workload
+import unittest
+from unittest.mock import MagicMock
+
+class MockConnector:
+    def __init__(self):
+        pass
+
+
+class TestCostEvaluation(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.db_name = "TestDB"
+
+        self.table = Table("TestTableA")
+        self.columns = [
+            Column("ColA", self.table),
+            Column("ColB", self.table),
+            Column("ColC", self.table),
+            Column("ColD", self.table),
+            Column("ColE", self.table)
+        ]
+
+        self.queries = [
+            Query(1, "SELECT * FROM TestTableA WHERE ColA = 4", [self.columns[0]]),
+            Query(2, "SELECT * FROM TestTableA WHERE ColB = 3", [self.columns[1]]),
+            Query(3, "SELECT * FROM TestTableA WHERE Col A = 14 AND ColB = 13", [self.columns[0], self.columns[1]]),
+        ]
+
+        self.workload = Workload(self.queries, self.db_name)
+
+        self.no_indexes = set()
+
+    def setUp(self):
+        self.connector = MockConnector()
+        self.connector.get_cost = MagicMock(return_value=3)
+        self.connector.simulate_index = MagicMock(return_value=[0, 1])
+        
+        self.cost_evaluation = CostEvaluation(self.connector)
+
+    def tearDown(self):
+        self.connector.simulate_index.reset_mock()
+        self.connector.get_cost.reset_mock()
+
+    def test_relevant_indexes(self):
+        index_A = Index([self.columns[0]])
+        index_B = Index([self.columns[1]])
+
+        result = self.cost_evaluation._relevant_indexes(self.queries[0], self.no_indexes)
+        self.assertEqual(result, frozenset())
+
+        result = self.cost_evaluation._relevant_indexes(self.queries[0], set([index_A]))
+        self.assertEqual(result, frozenset([index_A]))
+
+        result = self.cost_evaluation._relevant_indexes(self.queries[0], set([index_B, index_A]))
+        self.assertEqual(result, frozenset([index_A]))
+
+        result = self.cost_evaluation._relevant_indexes(self.queries[2], set([index_B, index_A]))
+        self.assertEqual(result, frozenset([index_B, index_A]))
+
+
+    def test_cost_requests(self):
+        self.assertEqual(self.cost_evaluation.cost_requests, 0)
+
+        CALCULATE_COST_CALLS = 3
+        for i in range(CALCULATE_COST_CALLS):
+            self.cost_evaluation.calculate_cost(self.workload, self.no_indexes)
+
+        expected_cost_requests = len(self.workload.queries) * CALCULATE_COST_CALLS
+        self.assertEqual(self.cost_evaluation.cost_requests, expected_cost_requests)
+
+        # Since we did not change the index configuration, all calls, except the first round, should be cached
+        expected_cache_hits = expected_cost_requests - len(self.workload.queries)
+        self.assertEqual(self.cost_evaluation.cache_hits, expected_cache_hits)
+
+    def test_cache_hit(self):
+        self.assertEqual(self.cost_evaluation.cost_requests, 0)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        workload = Workload([self.queries[0]], self.db_name)
+
+        self.cost_evaluation.calculate_cost(workload, self.no_indexes)
+        self.assertEqual(self.cost_evaluation.cost_requests, 1)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        self.cost_evaluation.calculate_cost(workload, self.no_indexes)
+        self.assertEqual(self.cost_evaluation.cost_requests, 2)
+        self.assertEqual(self.cost_evaluation.cache_hits, 1)
+
+    def test_no_cache_hit_unseen(self):
+        self.assertEqual(self.cost_evaluation.cost_requests, 0)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        workload = Workload([self.queries[0]], self.db_name)
+        index_A = Index([self.columns[0]])
+
+        self.cost_evaluation.calculate_cost(workload, self.no_indexes)
+        self.assertEqual(self.cost_evaluation.cost_requests, 1)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        self.cost_evaluation.calculate_cost(workload, set([index_A]))
+        self.assertEqual(self.cost_evaluation.cost_requests, 2)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+        self.connector.simulate_index.assert_called_with(index_A)
+
+
+    def test_cache_hit_non_relevant_index(self):
+        self.assertEqual(self.cost_evaluation.cost_requests, 0)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        workload = Workload([self.queries[0]], self.db_name)
+        index_B = Index([self.columns[1]])
+
+        self.cost_evaluation.calculate_cost(workload, self.no_indexes)
+        self.assertEqual(self.cost_evaluation.cost_requests, 1)
+        self.assertEqual(self.cost_evaluation.cache_hits, 0)
+
+        self.cost_evaluation.calculate_cost(workload, set([index_B]))
+        self.assertEqual(self.cost_evaluation.cost_requests, 2)
+        self.assertEqual(self.cost_evaluation.cache_hits, 1)
+        self.connector.simulate_index.assert_called_with(index_B)
+
+
+
+    # @classmethod
+    # def setUpClass(self):
+    #     self.db_name = 'tpch_test_db_database'
+    #     db = PostgresDatabaseConnector(None, autocommit=True)
+    #     table_generator = TableGenerator('tpch', 0.001, db, explicit_database_name=self.db_name)
+    #     db.close()
+
+    # @classmethod
+    # def tearDownClass(self):
+    #     connector = PostgresDatabaseConnector(None,
+    #                                                   autocommit=True)
+    #     if connector.database_exists(self.db_name):
+    #         connector.drop_database(self.db_name)
+
+
+    # def test_postgres_index_simulation(self):
+    #     db = PostgresDatabaseConnector(self.db_name, 'postgres')
+    #     self.assertTrue(db.supports_index_simulation())
+    #     db.close()
+
+    # def test_simple_statement(self):
+    #     db = PostgresDatabaseConnector(self.db_name, 'postgres')
+
+    #     statement = 'select count(*) from nation'
+    #     result = db.exec_fetch(statement)
+    #     self.assertEqual(result[0], 25)
+
+    #     db.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
