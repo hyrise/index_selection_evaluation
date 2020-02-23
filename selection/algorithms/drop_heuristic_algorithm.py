2351d1c9f5f1662f145effd37adccc635428c171
Jan Kossmann <jan.kossmann@hpi.de>
2020-02-23 19:22:58 +0100

    DROP: add pickling to trace index decisions

--- selection/algorithms/drop_heuristic_algorithm.py
+++ selection/algorithms/drop_heuristic_algorithm.py
@@ -1,6 +1,7 @@
 from ..selection_algorithm import SelectionAlgorithm
 import logging
 import itertools
+import pickle
 
 # Algorithm stops when maximum number of indexes is reached
 DEFAULT_PARAMETERS = {'max_indexes': 15, 'cost_estimation': 'whatif'}
@@ -12,6 +13,12 @@ class DropHeuristicAlgorithm(SelectionAlgorithm):
             parameters = {}
         SelectionAlgorithm.__init__(self, database_connector, parameters,
                                     DEFAULT_PARAMETERS)
+
+        self.log_index_history = False
+        self.index_drop_history = []
+        if 'log_index_history' in self.parameters:
+            assert self.parameters['max_indexes'] == 1, "log_index_history makes only sense for max_indexes = 1"
+            self.log_index_history = self.parameters['log_index_history']
 
     def _calculate_best_indexes(self, workload):
         assert self.parameters[
@@ -28,9 +35,17 @@ class DropHeuristicAlgorithm(SelectionAlgorithm):
             index_to_drop = None
             for index in remaining_indexes:
                 cost = self.cost_evaluation.calculate_cost(
-                    workload, remaining_indexes - set([index]))
+                    workload, remaining_indexes - set([index]), store_size=True)
                 if not lowest_cost or cost < lowest_cost:
                     lowest_cost, index_to_drop = cost, index
             remaining_indexes.remove(index_to_drop)
+
+            if self.log_index_history:
+                self.index_drop_history.append(index_to_drop)
+
+        if self.log_index_history:
+            # Add last remaining indexes
+            self.index_drop_history += list(remaining_indexes)
+            pickle.dump(self.index_drop_history, open(f"benchmark_results/drop_index_drop_history.pickle", "wb" ))
 
         return remaining_indexes
