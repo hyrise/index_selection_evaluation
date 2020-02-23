79d1628a9c14bb7dd451fa3d5cffd74c792e0882
Jan Kossmann <jan.kossmann@hpi.de>
2020-02-23 19:23:50 +0100

    MS: Add index history

--- selection/algorithms/microsoft_algorithm.py
+++ selection/algorithms/microsoft_algorithm.py
@@ -3,6 +3,7 @@ from ..workload import Workload
 from ..index import Index
 import itertools
 import logging
+import pickle
 
 # multi column index methods: 'no', 'lead', 'all'
 # cost_estimation: 'whatif' or 'acutal_runtimes'
@@ -22,6 +23,7 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
         self.max_indexes_naive = min(self.parameters['max_indexes_naive'],
                                      self.max_indexes)
         self.max_columns_per_index = self.parameters['max_index_columns']
+        self.index_history = []
 
     def _calculate_best_indexes(self, workload):
         logging.info('Calculating best indexes (microsoft)')
@@ -36,7 +38,7 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
                 1, self.max_columns_per_index + 1):
             candidates = self.select_index_candidates(workload,
                                                       potential_indexes)
-            indexes, _ = self.enumerate_combinations(workload, candidates)
+            indexes, _ = self.enumerate_combinations(workload, candidates, current_max_columns_per_index == self.max_columns_per_index)
             assert indexes & candidates == set(
             ), 'Intersection of indexes and candidate indexes must be empty'
 
@@ -44,6 +46,8 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
                 # Update potential indexes for the next iteration
                 potential_indexes = indexes | self.create_multicolumn_indexes(
                     workload, indexes)
+
+        pickle.dump(self.index_history, open(f"benchmark_results/microsoft_index_combination_history.pickle", "wb" ))
         return indexes
 
     def select_index_candidates(self, workload, potential_indexes):
@@ -69,7 +73,7 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
                 indexes.add(index)
         return indexes
 
-    def enumerate_combinations(self, workload, candidate_indexes):
+    def enumerate_combinations(self, workload, candidate_indexes, last_round=False):
         log_out = f'Start Enumeration\n' \
                   f'\tNumber of candidate indexes: {len(candidate_indexes)}\n' \
                   f'\tNumber of indexes to be selected: {self.max_indexes}'
@@ -89,7 +93,7 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
         number_indexes = min(self.max_indexes, len(candidate_indexes))
         indexes, costs = self.enumerate_greedy(workload, current_indexes,
                                                costs, candidate_indexes,
-                                               number_indexes)
+                                               number_indexes, last_round)
 
         log_out = f'lowest cost (greedy): {costs}\n' \
                   f'\tlowest cost indexes (greedy): {indexes}\n' \
@@ -115,7 +119,7 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
         return set(lowest_cost_indexes), lowest_cost
 
     def enumerate_greedy(self, workload, current_indexes, current_costs,
-                         candidate_indexes, number_indexes):
+                         candidate_indexes, number_indexes, last_round):
         assert current_indexes & candidate_indexes == set(
         ), 'Intersection of current and candidate indexes must be empty'
         if len(current_indexes) >= number_indexes:
@@ -135,12 +139,14 @@ class MicrosoftAlgorithm(SelectionAlgorithm):
             current_indexes.add(best_index[0])
             candidate_indexes.remove(best_index[0])
             current_costs = best_index[1]
+            if last_round:
+                self.index_history.append(best_index[0])
 
             logging.debug(f'Additional best index found: {best_index}')
 
             return self.enumerate_greedy(workload, current_indexes,
                                          current_costs, candidate_indexes,
-                                         number_indexes)
+                                         number_indexes, last_round)
         return current_indexes, current_costs
 
     def _simulate_and_evaluate_cost(self, workload, indexes):
