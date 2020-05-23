4895fb16e15c7cc2af8f36ba20c77019a57d6c56
Stefan Halfpap <klauck2@gmail.com>
2020-05-23 17:09:42 +0200

    Integrate index removal in relaxation algorithm

--- selection/algorithms/relaxation_algorithm.py
+++ selection/algorithms/relaxation_algorithm.py
@@ -31,14 +31,36 @@ class RelaxationAlgorithm(SelectionAlgorithm):
         # CP in Figure 5
         cp = candidates.copy()
         cp_size = sum(index.estimated_size for index in cp)
+        cp_cost = self.cost_evaluation.calculate_cost(workload, cp, store_size=True)
         while cp_size > self.disk_constraint:
             # Pick a configuration that can be relaxed
             # TODO: Currently only one is considered
 
             # Relax the configuration
             # TODO: Currently a random index is removed
-            removed_index = cp.pop()
-            cp_size -= removed_index.estimated_size
+            best_relaxed = None
+            best_relaxed_size = None
+            lowest_relaxed_penalty = None
+
+            # removal
+            for index in cp:
+                relaxed = cp.copy()
+                relaxed.remove(index)
+                relaxed_cost = self.cost_evaluation.calculate_cost(workload, relaxed, store_size=True)
+                relaxed_cost_increase = relaxed_cost - cp_cost
+                assert relaxed_cost_increase >= 0, 'Relaxed cost increase must be positive'
+                # any storage decrease beyond the disk_constraint is not considered
+                relaxed_storage_savings = min(index.estimated_size, cp_size - self.disk_constraint)
+                assert relaxed_storage_savings > 0, 'Relaxed storage savings must be positive'
+
+                if best_relaxed is None or lowest_relaxed_penalty > (relaxed_cost_increase / relaxed_storage_savings):
+                    # set new best relaxed configuration
+                    best_relaxed = relaxed
+                    best_relaxed_size = cp_size - index.estimated_size
+                    lowest_relaxed_penalty = (relaxed_cost_increase / relaxed_storage_savings)
+
+            cp = best_relaxed
+            cp_size = best_relaxed_size
 
         return list(cp)
 
--- tests/test_relaxation_algorithm.py
+++ tests/test_relaxation_algorithm.py
@@ -5,6 +5,7 @@ from tests.mock_connector import (
     MockConnector,
     mock_cache,
     column_A_0,
+    column_A_1,
     query_0,
     query_1,
 )
@@ -16,10 +17,36 @@ class TestRelaxationAlgorithm(unittest.TestCase):
         self.connector = MockConnector()
         self.database_name = "test_DB"
 
-    def test_calculate_indexes_1000MB_1column(self):
+    def test_possible_indexes(self):
         algorithm = RelaxationAlgorithm(
             database_connector=self.connector,
-            parameters={"max_indexes": 2, "budget": 1000},
+            parameters={"max_index_columns": 2, "budget": 1000},
+        )
+        result = algorithm._possible_indexes(query_0)
+        self.assertEqual(len(result), 1)
+
+        result = algorithm._possible_indexes(query_1)
+        self.assertEqual(len(result), 9)
+
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"max_index_columns": 3, "budget": 1000},
+        )
+        result = algorithm._possible_indexes(query_1)
+        self.assertEqual(len(result), 15)
+
+        # test with no parameter
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"budget": 1000},
+        )
+        result = algorithm._possible_indexes(query_1)
+        self.assertEqual(len(result), 15)
+
+    def test_calculate_indexes_3000MB_2column(self):
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"max_index_columns": 2, "budget": 3},
         )
         algorithm.cost_evaluation.cache = mock_cache
         algorithm.cost_evaluation._prepare_cost_calculation = (
@@ -27,14 +54,72 @@ class TestRelaxationAlgorithm(unittest.TestCase):
         )
         algorithm._exploit_virtual_indexes = lambda workload: (
             None,
-            set([Index([column_A_0], 100)]),
+            set([Index([column_A_0], 1000 * 1000), Index([column_A_0, column_A_1], 2000 * 1000)]),
         )
 
         index_selection = algorithm.calculate_best_indexes(
             Workload([query_0, query_1], self.database_name)
         )
-        self.assertEqual(set(index_selection), set([Index([column_A_0])]))
+        self.assertEqual(set(index_selection), set([Index([column_A_0]), Index([column_A_0, column_A_1])]))
 
+    def test_calculate_indexes_2MB_2column(self):
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"max_index_columns": 2, "budget": 2},
+        )
+        algorithm.cost_evaluation.cache = mock_cache
+        algorithm.cost_evaluation._prepare_cost_calculation = (
+            lambda indexes, store_size=True: None
+        )
+        algorithm._exploit_virtual_indexes = lambda workload: (
+            None,
+            set([Index([column_A_0], 1000 * 1000), Index([column_A_0, column_A_1], 2000 * 1000)]),
+        )
+
+        index_selection = algorithm.calculate_best_indexes(
+            Workload([query_0, query_1], self.database_name)
+        )
+        self.assertEqual(set(index_selection), set([Index([column_A_0, column_A_1])]))
+
+    def test_calculate_indexes_1MB_2column(self):
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"max_index_columns": 2, "budget": 1},
+        )
+        algorithm.cost_evaluation.cache = mock_cache
+        algorithm.cost_evaluation._prepare_cost_calculation = (
+            lambda indexes, store_size=True: None
+        )
+        algorithm._exploit_virtual_indexes = lambda workload: (
+            None,
+            set([Index([column_A_0], 1000 * 1000), Index([column_A_0, column_A_1], 2000 * 1000)]),
+        )
+
+        index_selection = algorithm.calculate_best_indexes(
+            Workload([query_0, query_1], self.database_name)
+        )
+        # The single column index is dropped first, because of the lower penalty.
+        # The multi column index is dropped second, because of space constraint.
+        self.assertEqual(set(index_selection), set())
+
+    def test_calculate_indexes_500kB_2column(self):
+        algorithm = RelaxationAlgorithm(
+            database_connector=self.connector,
+            parameters={"max_index_columns": 2, "budget": 0.5},
+        )
+        algorithm.cost_evaluation.cache = mock_cache
+        algorithm.cost_evaluation._prepare_cost_calculation = (
+            lambda indexes, store_size=True: None
+        )
+        algorithm._exploit_virtual_indexes = lambda workload: (
+            None,
+            set([Index([column_A_0], 1000 * 1000), Index([column_A_0, column_A_1], 2000 * 1000)]),
+        )
+
+        index_selection = algorithm.calculate_best_indexes(
+            Workload([query_0, query_1], self.database_name)
+        )
+        self.assertEqual(set(index_selection), set())
 
 if __name__ == "__main__":
     unittest.main()
