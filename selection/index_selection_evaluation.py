0267c5b5bf2a72901a19fe9cc6fc88684daa1a59
Jan Kossmann <jan.kossmann@hpi.de>
2020-06-19 11:50:14 +0200

    Lint and format

--- selection/index_selection_evaluation.py
+++ selection/index_selection_evaluation.py
@@ -154,8 +154,14 @@ class IndexSelection:
         logging.info("Indexes found: {}".format(indexes))
         what_if = algorithm.cost_evaluation.what_if
 
-        cost_requests = self.db_connector.cost_estimations if config["name"] == "ibm" else algorithm.cost_evaluation.cost_requests
-        cache_hits = 0 if config["name"] == "ibm" else algorithm.cost_evaluation.cache_hits
+        cost_requests = (
+            self.db_connector.cost_estimations
+            if config["name"] == "ibm"
+            else algorithm.cost_evaluation.cost_requests
+        )
+        cache_hits = (
+            0 if config["name"] == "ibm" else algorithm.cost_evaluation.cache_hits
+        )
         return indexes, what_if, cost_requests, cache_hits
 
     def create_algorithm_object(self, algorithm_name, parameters):
