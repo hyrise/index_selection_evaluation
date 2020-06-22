d270e3be8cfdc3c8ab765c5c7ce0729c5bf3b768
Jan Kossmann <jan.kossmann@hpi.de>
2020-06-22 20:18:02 +0200

    Some doc

--- selection/what_if_index_creation.py
+++ selection/what_if_index_creation.py
@@ -1,6 +1,8 @@
 import logging
 
-
+# Class that encapsulates simulated/WhatIf-Indexes.
+# This is usually used by the CostEvaluation class and there should be no need
+# to use it manually.
 # Uses hypopg for postgreSQL
 class WhatIfIndexCreation:
     def __init__(self, db_connector):
